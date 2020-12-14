fc07e905691ce4edc2893339a0e9b4e0a5bc466c
Zenessa Anderson <if0678lu@go.minnstate.edu>
2020-12-14 03:22:15 -0600

    3c //Unit Tests won't run.

--- 1730zanderson3c/Ex3cCalculations.cs
+++ 1730zanderson3c/Ex3cCalculations.cs
@@ -11,32 +11,85 @@ namespace _1730zanderson3c
         public static string Calc0(int index)
         {
             string[] days = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
-            return "";
+            string result = "Invalid input";
+            index = index - 1;
+            if (index >= 0 && index <= 7)
+                result = days[index];
+            else
+                result = "Invalid input";
+            
+            return result;
         }
 
-        public static string Calc1(string day)
+        public static string Calc1(string search)
         {
             string[] days = { "SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY" };
             string[] hours = { "Closed", "10am - 6pm", "10am - 6pm", "10am - 6pm", "10am - 9pm", "10am - 6pm", "8am - 4pm" };
-            return "";
+
+            search = search.Trim();
+            search = search.ToUpper();
+
+            string result = "Invalid input";
+
+            
+            int index = Array.IndexOf(days, search);
+            if (index >= 0)
+                result = hours[index];
+            else
+                result = "Invalid input";
+            
+            return result;
         }
 
         public static int Calc2(int[] numbers)
         {
-            return 1; 
+            int sum = 0;
+
+            foreach (int number in numbers)
+                sum += number;
+            return sum; 
         }
-        public static double Calc3(double[] numbers, int number)
+        public static double Calc3(double[] numbers, int count)
         {
-            return 0.0;
+            double sum = 0.0;
+
+            int length = numbers.GetLength(0);
+            if (length >= count)
+                for (int i = 0; i < count; i++)
+                    sum += numbers[i];
+            else
+                sum = 0.0;
+
+            return sum;
         }
         public static double Calc5(double[] numbers)
         {
-            return 0.0;
+            double avg = 0;
+            int count = Int32.Parse(inputListBox5a.Text);
+            if (count > 0)
+            {
+                double total = Ex3cCalculations.Calc3(numbers, count);
+                avg = total / Double.Parse(inputListBox4a.Items.Count.ToString());
+            }
+            else
+                MessageBox.Show("Invalid input" + "\n" + inputTextBox1a.Text);
+            return avg;                         //This needs to be powerwashed. I'm missing some major concept here
         }
 
         public static double[] Calc6(double[] numbers)
         {
-            return new double[10];
+            int length = numbers.GetLength(0);
+            List<Double> aboveAvgList = new List<double>();
+            if (length > 0)
+            {
+                double avg = Calc5(numbers);
+                foreach(int number in AboveAvg)
+                    if(numbers >= avg)
+                aboveAvgList.Add(numbers);
+            }
+            return aboveAvgList.ToArray();
+
+            
         }
     }
 }
--- 1730zanderson3c/MainWindow.xaml.cs
+++ 1730zanderson3c/MainWindow.xaml.cs
@@ -27,19 +27,94 @@ namespace _1730zanderson3c
 
         private void calcButton_Click(object sender, RoutedEventArgs e)
         {
+            //0
             try
             {
-                int index = 
+                int index = Int32.Parse(inputTextBox0a.Text);
+                resultTextBox0.Text = Ex3cCalculations.Calc0(index);
             }
             catch
             {
-
+                resultTextBox0.Text = "Invalid input";
+                this.inputTextBox0a.Focus();
             }
+
+            //1
+            try
+            {
+                string search = inputTextBox1a.Text;
+                resultTextBox1.Text = Ex3cCalculations.Calc1(search);
+            }
+            catch
+            {
+                MessageBox.Show("Invalid input" + "\n" + inputTextBox1a.Text);
+                this.inputTextBox1a.Focus();
+            }
+
+            //2
+            int[] numbers2 = { 11, 22, 23, 34, 10 };
+            resultTextBox2.Text = Ex3cCalculations.Calc2(numbers2).ToString();
+
+            //3
+            double[] numbers3 = { 11.1, 22.2, 23.3, 34.4, 9.0 };
+            int count = 0;
+            try
+            {
+                resultTextBox3.Text = "";
+                int count = Int32.Parse(inputTextBox3a.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Invalid input" + "\n" + inputTextBox3a.Text);
+                this.inputTextBox3a.Focus();
+            }
+
+            //4
+            double[] numbers4 = new double[inputListBox4a.Items.Count];
+            
+            for (int i = 0; i <count; i++)
+            numbers4[count] = Double.Parse(inputListBox4a.Items[count].ToString());
+
+            resultTextBox4.Text = Ex3cCalculations.Calc3(numbers4, count).ToString();
+
+            //5
+            double[] numbers5 = new double[inputListBox5a.Items.Count];
+            for (int i = 0; i < inputListBox5a.Items.Count; i++)
+                numbers5[i] = Double.Parse(inputListBox5a.Items.GetItemAt(i).ToString());
+            
+            resultTextBox5.Text = Ex3cCalculations.Calc5(numbers5).ToString("n1");
+
+            //6
+            double[] numbers6 = new double[inputListBox6a.Items.Count];
+            for (int i = 0; i < inputListBox6a.Items.Count; i++)
+            {
+                numbers6[i] = Double.Parse(inputListBox6a.Items.GetItemAt(i).ToString());
+            }
+            double[] AboveAvg = Ex3cCalculations.Calc6(numbers6);
+
+            foreach (int number in AboveAvg)
+            {
+                int i = 0;
+                i = numbers6.GetLength(0);
+                resultListBox6.Items.Add(AboveAvg[i]);
+            }
+
         }
 
         private void addItemButton4_Click(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                double input = Double.Parse(this.inputTextBox4a.Text);
+                this.inputListBox4a.Items.Add(input.ToString("n1"));
+                this.resultTextBox4.Text = "";
+                this.inputListBox4a.ScrollIntoView(inputListBox4a.Items[inputListBox4a.Items.Count - 1]);
+            }
+            catch
+            {
+                MessageBox.Show("Invalid input" + "\n" + inputTextBox4a.Text);
+                this.inputListBox4a.Focus();
+            }
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -61,6 +136,13 @@ namespace _1730zanderson3c
             inputListBox6a.Items.Add("23.3");
             inputListBox6a.Items.Add("34.4");
             inputListBox6a.Items.Add("9.0");
+
+            inputTextBox0a.Focus();
+        }
+
+        private void inputTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            ((TextBox)sender).SelectAll();
         }
     }
 }
